51afa4cad264c7a04a43298bc2d67c2344449d7b
jokyo02 <149650929+jokyo02@users.noreply.github.com>
2023-11-11 16:16:11 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
-//const _U = '1NssJY8JoQgpNNg8fXA66mbbkV5Ev0YAin-YBPurJ69Zsa0gQ-Bq6eINo9NBQ7tam-OM4pykcZqsteI91B6BNM2AgD1XLp8Xy_yL34R2xDEGsb1V-lpIysooyQOlfECAtk_VJRtKUUWLXOI2TLVpfsrqcQhPGF56o56yge4LI11oy7q3K2v2zRSe6c6kpwP6bwtGVJF6cNtmxgzaf0UTEuw; WLS=C=4f32388253816ce8&N=cf03';//cf03
-
 // 导入express模块
 const express = require('express');
+// 导入ws模块
+const WebSocket = require('ws');
+
 // 创建一个express应用
 const app = express();
+
+// 创建一个WebSocket服务器
+const wss = new WebSocket.Server({ noServer: true });
+
+// 处理GET /请求的路由
+app.get('/', (req, res) => {
+  // 返回一个简单的响应
+  res.send('Hello, world!');
+});
 
 // 设置跨域访问的中间件
 app.use((req, res, next) => {
@@ -35,11 +45,11 @@ app.use((req, res, next) => {
 async function handleWebSocket(req, res) {
   // 如果需要，可以将serverUrl替换为您的服务器地址
   let serverUrl = "https://sydney.bing.com";
-   let fetchUrl = new URL(request.url);
-   fetchUrl.hostname = serverUrl;
+  // let fetchUrl = new URL(request.url);
+  // fetchUrl.hostname = serverUrl;
 
-//  const currentUrl = new URL(req.url);
-//  const fetchUrl = new URL(serverUrl + currentUrl.pathname + currentUrl.search);
+  const currentUrl = new URL(req.url);
+  const fetchUrl = new URL(serverUrl + currentUrl.pathname + currentUrl.search);
 
   let serverRequest = new Request(fetchUrl, req);
   // serverRequest.headers.set('Host', 'sydney.bing.com');
@@ -55,28 +65,25 @@ async function handleWebSocket(req, res) {
   serverRequest.headers.set('Cookie', cookies);
 
   const response = await fetch(serverRequest);
-  const newRes = new Response(response.body, response);
-
-  //newRes.headers.set('Access-Control-Allow-Origin', request.headers.get('Origin'));
-  newRes.headers.set('Access-Control-Allow-Methods', 'GET,HEAD,POST,OPTIONS');
-  newRes.headers.set('Access-Control-Allow-Credentials', 'true');
-  newRes.headers.set('Access-Control-Allow-Headers', '*');
-
-   // 创建一个新的URL对象，指向http://ipecho.net/plain
-//   let Ipurl = new URL("http://ipecho.net/plain")
-   // 使用fetch函数获取该URL的响应
- //  let Ipresponse = await fetch(Ipurl)
-   // 如果响应状态码为200，表示成功
-//   if (Ipresponse.status == 200) {
-     // 获取响应的文本内容
- //    let textip = await Ipresponse.text()
-//newRes.headers.set('TestLog',"This is Sydney@" + textip);
-
-//const Guestip = req.headers['cf-connecting-ip'];
-//newRes.headers.set('Guestip',Guestip); 
-
-  // 将新的响应发送给客户端
-  res.send(newRes);
+  
+  // 如果服务器返回了101状态码，表示升级协议成功
+  if (response.status === 101) {
+    // 使用wss.handleUpgrade方法来建立WebSocket连接
+    wss.handleUpgrade(req, res.socket, Buffer.alloc(0), (ws) => {
+      // 将服务器返回的响应头设置到WebSocket对象上
+      ws.headers = response.headers;
+      // 创建一个双向流来转发数据
+      const duplex = WebSocket.createWebSocketStream(ws);
+      // 将服务器返回的响应体转发到客户端
+      response.body.pipe(duplex);
+      // 将客户端发送的数据转发到服务器
+      duplex.pipe(response.body);
+    });
+  } else {
+    // 如果服务器返回了其他状态码，表示升级协议失败
+    // 将服务器返回的响应发送给客户端
+    res.send(response);
+  }
 }
 
 // 监听3000端口
