b6618adebca17c3caafdf17c4efe9eeaf2cfee4e
jokyo02 <149650929+jokyo02@users.noreply.github.com>
2023-11-11 15:27:07 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
-const request = require('request');
+const app = express();
+const https = require('https');
+const http = require('http');
+const { URL } = require('url');
 
 const SYDNEY_ORIGIN = 'https://sydney.bing.com';
 const KEEP_REQ_HEADERS = [
@@ -20,55 +23,86 @@ const KEEP_REQ_HEADERS = [
   'access-control-request-method',
 ];
 
-const app = express();
+app.use((req, res, next) => {
+  const currentUrl = new URL(req.url, `http://${req.headers.host}`);
+  const targetUrl = new URL(SYDNEY_ORIGIN + currentUrl.pathname + currentUrl.search);
 
-// Handle all requests
-app.use((req, res) => {
-  // Get the target url from the request url
-  const targetUrl = SYDNEY_ORIGIN + req.url;
-
-  // Filter and modify the headers you want
-  const headers = {};
-  for (const key of Object.keys(req.headers)) {
-    if (KEEP_REQ_HEADERS.includes(key)) {
-      headers[key] = req.headers[key];
+  const newHeaders = {};
+  for (let key of KEEP_REQ_HEADERS) {
+    if (req.headers[key]) {
+      newHeaders[key] = req.headers[key];
     }
   }
-//  headers['origin'] = SYDNEY_ORIGIN;
-  headers['origin'] = 'https://www.bing.com';
-  headers['referer'] = 'https://www.bing.com/search?q=Bing+AI';
- // const randIP = '163.47.101.101';
- // headers['X-Forwarded-For'] = randIP;
 
-  const cookie = req.headers['cookie'] || '';
+  newHeaders['origin'] = targetUrl.origin;
+  newHeaders['referer'] = 'https://www.bing.com/search?q=Bing+AI';
+
+//    const randIP = '163.47.101.101';
+//    newHeaders['X-Forwarded-For'] = randIP;
+
+  const cookie = req.headers['Cookie'] || '';
   let cookies = cookie;
-  if (!cookie.includes('_U=')) {
-    cookies += '; _U=' + '';
-  }
-  headers['cookie'] = cookies;
+  
+//    if (!cookie.includes('KievRPSSecAuth=')) {
+  
+ //       cookies += '; KievRPSSecAuth=' + '';
+ //     }
+  
+//    if (!cookie.includes('_RwBf=')) {
 
-  // Send the request to the target url with the modified headers
-  request(
-    {
-      url: targetUrl,
-      method: req.method,
-      headers: headers,
-      body: req.body,
-    },
-    (error, response, body) => {
-      if (error) {
-        // Handle error
-        console.error(error);
-        res.status(500).send('Something went wrong');
-      } else {
-        // Send the response back to the client
-        res.status(response.statusCode).set(response.headers).send(body);
-      }
+ //       cookies += '; _RwBf=' +'';
+ //     }
+  
+//    if (!cookie.includes('MUID=')) {
+
+//          cookies += '; MUID=' + '074AD7F106536BC6392FC4C907CA6AEA';
+//        }
+    
+  if (!cookie.includes('_U=')) {
+
+      cookies += '; _U=' + '';
     }
-  );
+  
+    newHeaders['Cookie'] = cookies;
+
+//    const oldUA = req.headers['user-agent'];
+//    const isMobile = oldUA.includes('Mobile') || oldUA.includes('Android');
+ //   if (isMobile) {
+ //     newHeaders['user-agent'] =
+//        'Mozilla/5.0 (iPhone; CPU iPhone OS 15_7 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.7 Mobile/15E148 Safari/605.1.15 BingSapphire/1.0.410427012';
+ //   } else {
+//      newHeaders['user-agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36 Edg/113.0.1774.35';
+ //   }
+
+  const options = {
+    method: req.method,
+    headers: newHeaders,
+    hostname: targetUrl.hostname,
+    path: targetUrl.pathname + targetUrl.search,
+    port: targetUrl.port,
+    protocol: targetUrl.protocol,
+  };
+
+  let proxy;
+  if (targetUrl.protocol === 'https:') {
+    proxy = https.request(options);
+  } else {
+    proxy = http.request(options);
+  }
+
+  proxy.on('response', (proxyRes) => {
+    res.writeHead(proxyRes.statusCode, proxyRes.headers);
+    proxyRes.pipe(res, { end: true });
+  });
+
+  proxy.on('error', (err) => {
+    console.error(err);
+    res.status(500).send('Something went wrong');
+  });
+
+  req.pipe(proxy, { end: true });
 });
 
-// Start the server
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server is running');
 });
