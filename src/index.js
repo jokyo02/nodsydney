7990a8c5a8d10dc5dc1fd01b88cc20e4d7f259ae
jokyo02 <149650929+jokyo02@users.noreply.github.com>
2023-11-18 08:39:03 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 //这样，当你访问 http://localhost:7860/ 时，就会看到'Site is working'的提示。希望这能帮到你。😊
 
 const httpProxy = require('http-proxy');
-const https = require('https');
+//const https = require('https');
 const proxy = httpProxy.createProxyServer({});
 
 proxy.on('proxyReq', function(proxyReq, req, res, options) {
@@ -11,8 +11,8 @@ proxy.on('proxyReq', function(proxyReq, req, res, options) {
 
 const serverUrl = 'sydney.bing.com';
 
-//const server = require('http').createServer(function(req, res) {
-const server = https.createServer(options, function(req, res) {
+const server = require('http').createServer(function(req, res) {
+//const server = https.createServer(options, function(req, res) {
   // 添加一个判断条件
   if (req.url === '/') {
     // 返回一个简单的消息
