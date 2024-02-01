897af7cd35a11f1eeca97f73de47edc21e7f71e3
jokyo02 <149650929+jokyo02@users.noreply.github.com>
2024-02-01 20:21:25 +0800

    Update oldindex.js

--- oldindex.js
+++ oldindex.js
@@ -6,10 +6,10 @@ const uwsReverseProxy = require('uws-reverse-proxy');
 const app = express();
 
 // 设置端口
-const PORT = process.env.PORT || 3000; // Render 会提供环境变量 PORT
+const PORT = process.env.PORT || 7860; // Render 会提供环境变量 PORT
 
 // 创建一个反向代理，将请求转发到 https://sydney.bing.com
-const proxy = uwsReverseProxy.createServer({ target: 'https://sydney.bing.com' });
+const proxy = uwsReverseProxy.createServer({ target: 'https://testwebing.nbing.eu.org' });
 
 // 定义一个简单的路由来响应 GET 请求
 app.get('/', (req, res) => {
